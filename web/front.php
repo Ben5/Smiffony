cf2022ff65f71cd808204ada0520efbd999aa410
Ben5 <ben-smith@blueyonder.co.uk>
2013-01-02 12:44:56 +0000

    Adding dependency injection to the framework. src/container.php is where the dependecies are specified.

--- web/front.php
+++ web/front.php
@@ -4,33 +4,21 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel;
-use Symfony\Component\Routing;
+use Symfony\Component\DependencyInjection\Reference;
+
+$sc       = include __DIR__.'/../src/container.php';
+
+// register custom listeners here:
+$sc->register('listener.string_response', 'Smiffony\StringResponseListener');
+$sc->getDefinition('dispatcher')
+    ->addMethodCall('addSubscriber', array(new Reference('listener.string_response')));
+
+// set up any config params in the dependency-injection container
+$sc->setParameter('charset', 'UTF-8');
+$sc->setParameter('routes', include __DIR__.'/../src/app.php');
 
 $request  = Request::createFromGlobals();
-$routes   = include __DIR__.'/../src/app.php';
 
-$context  = new Routing\RequestContext();
-$matcher  = new Routing\Matcher\UrlMatcher($routes, $context);
-$resolver = new HttpKernel\Controller\ControllerResolver();
-
-$dispatcher = new EventDispatcher();
-// add a listener that populates request attributes with route parameters
-$dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher));
-// add a listener that handles exceptions in a nice way
-$exceptionListener = new HttpKernel\EventListener\ExceptionListener('Calendar\\Controller\\ErrorController::exceptionAction');
-$dispatcher->addSubscriber($exceptionListener);
-// add a listener that prepares the Response before sending it out
-$dispatcher->addSubscriber(new HttpKernel\EventListener\ResponseListener('UTF-8'));
-// add a listener that allows controllers to return a string instead of a Response (the listener creates a Response from the string)
-$dispatcher->addSubscriber(new Smiffony\StringResponseListener());
-
-// now create the framework!
-$framework = new Smiffony\Framework($dispatcher, $resolver);
-
-// let the framework handle the request and generate (and send) a response
-$response = $framework->handle($request);
+$response = $sc->get('framework')->handle($request);
 $response->send();
